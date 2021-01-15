0f107fb3222b657dbe6d37c9c2b0079ba2877cf6
Bethany Green <b.gree10@gail.com>
2021-01-15 13:26:50 -0600

    started working on the index file and added inquirer

--- index.js
+++ index.js
@@ -1,13 +1,56 @@
-// TODO: Include packages needed for this application
+const fs = require('fs');
+const path = require('path');
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown');
 
-// TODO: Create an array of questions for user input
-const questions = [];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
 
-// TODO: Create a function to initialize app
-function init() {}
-
-// Function call to initialize app
-init();
+const promptUser = () => 
+inquirer.prompt([//spits out the messages and prompts in the terminal
+     {
+         type: 'input',
+         message: 'What is your Github username?',
+         name: 'name',
+     },
+     {
+         type:'input',
+         message:'What is your email address?',
+         name:'email',
+     },
+     {
+        type:'input',
+        message:'What is your project`s name?',
+        name:'project',
+     },
+     {
+        type:'input',
+        message:'Please write a short description of your project.',
+        name:'description',
+    },
+    {
+       type:'input',
+       message:'What kind of license should your project have? ',
+       name:'github',
+    },
+    {
+        type:'input',
+        message:'What command s hould be run to install dependencies?',
+        name:'command',
+    },
+    {
+       type:'input',
+       message:'What command should be run to run tests? ',
+       name:'tests',
+    },
+    {
+        type:'input',
+        message:'What does the user need to know about using the repo?',
+        name:'repo',
+    },
+    {
+        type:'input',
+        message:'What does the user need to know about contributing to the repo?',
+        name:"contribute",
+    },
+ ])
+ 
